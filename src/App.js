b9eed7a3c1f79d14bba26c7509eb590347e5c3c2
VaishnaviMudaliar <vaishnavim1311@gmail.com>
2022-04-08 15:32:35 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <div>
       <h1>Upload Resources</h1>
-      <p>Start editing to see some magic happen :)</p>
+    
       <FileUploader />
       <ToastContainer />
     </div>
